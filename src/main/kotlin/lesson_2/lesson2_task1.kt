5bd1fb68e7a4224afc3a1ab5adb54390669e6308
boss <vodochnik1982@gmail.com>
2025-04-03 14:26:09 +0300

    KS-2-1 task1.

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+
+    val score_1: Float = 3f
+    val score_2: Float = 4f
+    val score_3: Float = 3f
+    val score_4: Float = 5f
+    val numberOfStudents = 4
+    val averageScore = (score_1 + score_2 + score_3 + score_4) / numberOfStudents
+
+    println(averageScore)
+}
