bfee327444bb773f4e192ec65661d46cc1ade618
boss <vodochnik1982@gmail.com>
2025-04-03 13:46:24 +0300

    KS-1-5 task5. Ошибки исправил. Форматирование времени не нашел, но найду.

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -5,14 +5,14 @@ package lesson_1
 //Взлет – 09:07
 //Посадка – 10:55
 
-const val secondsInHour = 3600
-const val secondsInMinute = 60
+const val SecondsInHour = 3600
+const val SecondsInMinute = 60
 
 fun main() {
     val gagarinInSpaceInSec = 6480
     println(gagarinInSpaceInSec)
-    val hours = gagarinInSpaceInSec / secondsInHour
-    val minutes = (gagarinInSpaceInSec - hours * secondsInHour) / secondsInMinute
+    val hours = gagarinInSpaceInSec / SecondsInHour
+    val minutes = (gagarinInSpaceInSec - hours * SecondsInHour) / SecondsInMinute
     println("0$hours:$minutes:00")
 
 }
