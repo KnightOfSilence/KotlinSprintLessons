67a49b8dee05355eea23091e188d4824e92f91cb
boss <vodochnik1982@gmail.com>
2025-04-04 23:34:00 +0300

    KS-2-3 task3

--- src/main/kotlin/lesson_2/Lesson2_task3.kt
+++ src/main/kotlin/lesson_2/Lesson2_task3.kt
@@ -6,16 +6,11 @@ fun main() {
     val startHour = 9
     val startMinutes = 39
     val timeOnRouteInMinutes = 457
-    val timeInDistanceInHours: Int
-    val timeInDistanceInMinutes: Int
-    val resHours: Int
-    val resMinutes: Int
+    val timeInDistanceInHours: Int = timeOnRouteInMinutes / MINUTES_IN_HOUR
+    val timeInDistanceInMinutes = timeOnRouteInMinutes % MINUTES_IN_HOUR
+    val resHours = startHour + timeInDistanceInHours
+    val resMinutes = startMinutes + timeInDistanceInMinutes
     val res = "17:16"
-
-    timeInDistanceInHours = timeOnRouteInMinutes / MINUTES_IN_HOUR
-    timeInDistanceInMinutes = timeOnRouteInMinutes % MINUTES_IN_HOUR
-    resHours = startHour + timeInDistanceInHours
-    resMinutes = startMinutes + timeInDistanceInMinutes
 
     println(resHours + 1)
     println(resMinutes - MINUTES_IN_HOUR)
