490415ed423f0daac5a1739f719f8eaf607958de
boss <vodochnik1982@gmail.com>
2025-04-04 18:33:26 +0300

    KS-2-3 task3. Исправление ошибок.

--- src/main/kotlin/lesson_2/Lesson2_task3.kt
+++ src/main/kotlin/lesson_2/Lesson2_task3.kt
@@ -1,21 +1,23 @@
 package lesson_2
 
+const val MINUTES_IN_HOUR = 60
+
 fun main() {
     val startHour = 9
     val startMinutes = 39
     val timeOnRouteInMinutes = 457
-    val timeInDistanceInHours: Int = timeOnRouteInMinutes / 60
-    val timeInDistanceInMinutes = timeOnRouteInMinutes % 60
-    val resHours = startHour + timeInDistanceInHours
-    val resMinutes = startMinutes + timeInDistanceInMinutes
+    val timeInDistanceInHours: Int
+    val timeInDistanceInMinutes: Int
+    val resHours: Int
+    val resMinutes: Int
     val res = "17:16"
 
+    timeInDistanceInHours = timeOnRouteInMinutes / MINUTES_IN_HOUR
+    timeInDistanceInMinutes = timeOnRouteInMinutes % MINUTES_IN_HOUR
+    resHours = startHour + timeInDistanceInHours
+    resMinutes = startMinutes + timeInDistanceInMinutes
+
     println(resHours + 1)
-    println(resMinutes - 60)
+    println(resMinutes - MINUTES_IN_HOUR)
     println(res)
-
-
-
-
-
 }
