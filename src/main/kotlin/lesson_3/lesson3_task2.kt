54ea59f4cd863303680bc5165719297d0a51b90f
boss <vodochnik1982@gmail.com>
2025-04-05 11:12:02 +0300

    KS-2-3 task3

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -2,13 +2,15 @@ package lesson_3
 
 fun main() {
 
-    val surName = "Андреева"
+    var surName = "Андреева"
     val firstName = "Татьяна"
     val secondName = "Сергеевна"
-    val surNameNew = "Сидорова"
     val ageFirst = 20
     val ageSecond = 22
 
     println("$surName $firstName $secondName, $ageFirst лет")
-    println("$surNameNew $firstName $secondName, $ageSecond лет")
+
+    surName = "Сидорова"
+
+    println("$surName $firstName $secondName, $ageSecond лет")
 }
