8ec7f0a4848b4d96944e1a5e71eeeffee412f056
boss <vodochnik1982@gmail.com>
2025-04-09 12:15:58 +0300

    KS-4-3 task3. Исправления.

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,7 +1,7 @@
 package lesson_4
 
-const val SUNNY_WEATHER = true
-const val TENT_IS_OPEN = true
+const val IS_WEATHER_SUNNY = true
+const val IS_TENT_OPEN = true
 const val AIR_HUMIDITY = 20
 const val TIME_OF_THE_YEAR = "зима"
 
@@ -12,8 +12,8 @@ fun main() {
     val airHmd = 20
     val timeOfTheYear = "зима"
 
-    val res = isWeatherSunny == SUNNY_WEATHER && isTentOpen == TENT_IS_OPEN && airHmd == AIR_HUMIDITY
-            && timeOfTheYear == TIME_OF_THE_YEAR
+    val res = isWeatherSunny == IS_WEATHER_SUNNY && isTentOpen == IS_TENT_OPEN && airHmd == AIR_HUMIDITY
+            && timeOfTheYear != TIME_OF_THE_YEAR
     println("Благоприятные ли условия сейчас для роста бобовых? $res")
 }
 
