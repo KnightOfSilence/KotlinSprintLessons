4ae4c91aac223471d1433c3e29eb99460f023c34
boss <vodochnik1982@gmail.com>
2025-06-06 23:47:45 +0300

    KS-5-5 lottery_pro.

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -2,6 +2,26 @@ package lesson_5
 
 fun main() {
 
+    val number1 = (0..42).random()
+    val number2 = (0..42).random()
+
+    println("Введите первое число от 0 до 42")
+    val userFirstNum = readln().toInt()
+    println("Введите второе число от 0 до 42")
+    val userSecondNum = readln().toInt()
+
+    if ((userFirstNum == number1 || userFirstNum == number2) &&
+        (userSecondNum == number1 || userSecondNum == number2)){
+        println("Поздравляем! Вы выиграли главный приз!")
+    }
+    else if ((userFirstNum == number1 || userFirstNum == number2) ||
+        (userSecondNum == number1 || userSecondNum == number2)){
+        println("Вы выиграли утешительный приз!")
+    }
+    else {
+        println("Неудача!")
+    }
+    println("Выйгрышные числа $number1 и $number2")
 }
 
 
