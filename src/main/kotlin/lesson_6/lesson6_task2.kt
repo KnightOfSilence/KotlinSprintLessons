bd940c065ea1a24c1773080b1d1781150fce4893
boss <vodochnik1982@gmail.com>
2025-07-27 15:37:31 +0300

    KS-6-2 timer

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -3,15 +3,7 @@ package lesson_6
 fun main() {
 
     println("Введите количество секунд")
-    val seconds = readln().toInt()
-    var counter = seconds
-
-    do {
-        counter--
-        Thread.sleep(1000)
-
-    } while (counter > 0)
-
+    val seconds = readln().toLong()
+    Thread.sleep(seconds * 1000)
     println("Прошло $seconds секунд")
-
 }
