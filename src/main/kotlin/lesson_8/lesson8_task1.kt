845ebf49579f72f87c2d18cfe200fb1e0b0800ca
boss <vodochnik1982@gmail.com>
2025-07-29 20:35:04 +0300

    KS-8-1 view number

---
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -0,0 +1,13 @@
+package lesson_8
+
+fun main(){
+
+    val arrayOfNumbers = arrayOf(154, 642, 342, 987, 543, 675, 999)
+    var totalSum = 0
+
+    for (i in arrayOfNumbers){
+        totalSum += i
+    }
+
+    println(totalSum)
+}
