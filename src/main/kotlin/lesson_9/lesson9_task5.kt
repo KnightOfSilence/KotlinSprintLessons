bf133b33a926183463db2e2facbaf39fc7990031
boss <vodochnik1982@gmail.com>
2025-07-31 18:59:02 +0300

    KS-9-5

---
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -0,0 +1,17 @@
+package lesson_9
+
+fun main() {
+
+    var ingredientsList = mutableListOf<String>()
+
+    for (i in 1..5) {
+        println("Введите название ингредиента $i:")
+        val  userInput = readln()
+        ingredientsList.add(userInput)
+    }
+    val ingredientsSet = ingredientsList.toSet()
+    ingredientsList = ingredientsSet.toMutableList()
+    val sortedIngredientsList = ingredientsList.sorted()
+
+    println(sortedIngredientsList.joinToString(", "))
+}
