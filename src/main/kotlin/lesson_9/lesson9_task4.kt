7eb28472b79f7bfbf4aa5ef8a182c8d2914ea811
boss <vodochnik1982@gmail.com>
2025-07-31 18:02:22 +0300

    KS-9-4 sorted_list

---
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -0,0 +1,12 @@
+package lesson_9
+
+fun main() {
+
+    println("Введите пять ингредиентов через запятую с пробелом:")
+    val userInput = readln()
+    val ingredientsList = userInput.split(",")
+    val sortedIngredientsList = ingredientsList.sorted()
+
+    println("Следующие ингредиенты добавлены в список:")
+    sortedIngredientsList.forEach { println(it) }
+}
