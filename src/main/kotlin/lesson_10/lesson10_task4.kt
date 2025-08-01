aac9bcc233169c8af19fdd99a2326b1b5bead32f
boss <vodochnik1982@gmail.com>
2025-08-01 15:33:52 +0300

    KS-10-4 dice game pro

---
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -0,0 +1,42 @@
+package lesson_10
+
+fun main() {
+
+    var userWins = 0
+    var continueGame = true
+
+    while (continueGame) {
+
+        val userTurn: Int = userRollingDice()
+        println("У игрока выпало число $userTurn")
+        val computerTurn: Int = computerRollingDice()
+        println("У компьютера выпало число $computerTurn")
+
+        if (userTurn > computerTurn) {
+            println("Победило человечество")
+            userWins++
+        } else if (userTurn < computerTurn) println("Победила машина")
+        else println("Победила дружба")
+
+        continueGame = newRound()
+    }
+
+    println("Игра окончена. Вы выиграли $userWins раз.")
+}
+
+fun userRollingDice(): Int {
+    return (1..6).random()
+}
+
+fun computerRollingDice(): Int {
+    return (1..6).random()
+}
+
+fun newRound(): Boolean {
+
+    println("Хотите сыграть еще раз? Введите Да или Нет: ")
+    val userAnswer = readln().toString().lowercase()
+
+    if (userAnswer == "да") return true
+    else return false
+}
