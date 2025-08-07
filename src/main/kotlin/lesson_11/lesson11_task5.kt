002d8006b7a92be97279edacc53bcb7ed1d088fc
boss <vodochnik1982@gmail.com>
2025-08-07 20:12:10 +0300

    KS-11-5 Проверяем правильность классов ForumMember и ForumPost

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -1,26 +1,33 @@
 package lesson_11
 
-
 class ForumMember(
-    val userId: Int?,
-    val userName: String?,
-    val nameList: List<String>,
-    )
-class ForumMemberBuilder {
-    var userId: Int? = null
-    var userName: String? = null
-    var nameList = mutableListOf<String>()
-
-    fun userId(value: Int) = apply { this.userId = value }
-    fun userName(value: String) = apply { this.userName = value }
-    fun addNameList(value: String) = apply { this.nameList.add(value) }
-    fun build(): ForumMember {
-        return ForumMember(userId, userName, nameList.toList())
+    val userId: Int,
+    val userName: String,
+) {
+    class ForumMemberBuilder(
+        var userId: Int,
+        var userName: String,
+    ) {
+        fun userId(value: Int) = apply { this.userId = value }
+        fun userName(value: String) = apply { this.userName = value }
+        fun build(): ForumMember {
+            return ForumMember(userId, userName)
+        }
     }
-
-
 }
+
 class ForumPost(
     val authorId: Int,
     val message: String,
-)
+) {
+    class ForumPostBuilder(
+        var authorId: Int,
+        var message: String,
+    ) {
+        fun authorId(value: Int) = apply { this.authorId = value }
+        fun message(value: String) = apply { this.message = value }
+        fun build(): ForumPost {
+            return ForumPost(authorId, message)
+        }
+    }
+}
