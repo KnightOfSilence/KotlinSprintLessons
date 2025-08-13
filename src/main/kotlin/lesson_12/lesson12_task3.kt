7eaccbf1dfc3b1165eccd9b424035096eedb0180
BOSS <vodochnik1982@gmail.com>
2025-08-13 13:21:31 +0300

    KS-12-3

--- src/main/kotlin/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -1,2 +1,23 @@
 package lesson_12
 
+
+const val KELVIN_TO_CELCIUS = 273.15
+class DailyWeather2(_dailyTemp: Int, _nightTemp: Int, _hasPrecipitation: Boolean) {
+
+    val dailyTemp = _dailyTemp - KELVIN_TO_CELCIUS
+    val nightTemp = _nightTemp - KELVIN_TO_CELCIUS
+    val hasPrecipitation = _hasPrecipitation
+
+    fun printWeather2() {
+        println("Daily temperature: ${dailyTemp.format(2)} °C")
+        println("Night temperature: ${nightTemp.format(2)} °C")
+        println("Has precipitation: $hasPrecipitation")
+    }
+}
+
+fun Double.format(digits: Int) = "%.${digits}f".format(this)
+
+fun main() {
+    val weather2 = DailyWeather2(280, 250, false)
+    weather2.printWeather2()
+}
