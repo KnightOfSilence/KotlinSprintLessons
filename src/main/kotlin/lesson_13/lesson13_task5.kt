ce0fd17c8e684254ccdfc636c7f2924d00cfb585
boss <vodochnik1982@gmail.com>
2025-08-18 00:40:37 +0300

    KS-13-5

---
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -0,0 +1,44 @@
+package lesson_13
+
+
+class PhoneBook4(
+    val name: String,
+    val phoneNumber: String,
+    val company: String? = null,
+) {
+    fun printInfo() {
+        val companyInfo = company ?: "<не указано>"
+        println(
+            "- Имя: $name\n" + "- Номер: $phoneNumber\n" + "- $companyInfo"
+        )
+    }
+}
+
+fun main() {
+    val contactList = mutableListOf<PhoneBook4>()
+
+
+    fun addContact(name: String, phoneNumber: String, company: String? = null) {
+        println("Введите номер телефона:")
+        try {
+            val phoneNumber = phoneNumber.toLong()
+        } catch (e: NumberFormatException) {
+            // Ловим ошибку, если ввод не является числом
+            println("Ошибка: $e")
+            println("Номер телефона должен содержать только цифры!")
+            return
+            if (phoneNumber == null) {
+                println("Вы не указали номер телефона.")
+                return
+            }
+
+            val contact = PhoneBook4(name, phoneNumber, company)
+            contactList.add(contact)
+        }
+    }
+
+    addContact("Петр", "неверный номер", "ООО Вега")
+    addContact("Елена", "", "ООО Альфа")
+}
+
+
