9c5004a4d54db30ef9e08963641dc0cb7ab6d2af
boss <vodochnik1982@gmail.com>
2025-08-18 00:46:24 +0300

    KS-13-4

---
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -0,0 +1,44 @@
+package lesson_13
+
+
+class PhoneBook4(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+) {
+    fun printInfo() {
+        val companyInfo = company ?: "<не указано>"
+        println(
+            "- Имя: $name\n" + "- Номер: $phoneNumber\n" + "- $companyInfo"
+        )
+    }
+}
+
+fun main() {
+    val contactList = mutableListOf<PhoneBook4>()
+
+    fun addContact(name: String, phoneNumber: String, company: String? = null) {
+//        println("Введите имя контакта:")
+//        val name = name
+        println("Введите номер телефона:")
+        val phoneNumber = phoneNumber.toLongOrNull()
+        if (phoneNumber == null) {
+            println("Вы не указали номер телефона.")
+            return
+        }
+//        println("Введите название компании:")
+//        val company = company
+        val contact = PhoneBook4(name, phoneNumber, company)
+        contactList.add(contact)
+    }
+
+    addContact("Иван", "89123456789", "ООО Ромашка")
+    addContact("Анна", "89012345678")
+    addContact("Петр", "неверный номер", "ООО Вега")
+    addContact("Мария", "89345678901", null)
+    addContact("Сергей", "89456789012", "null")
+    addContact("Елена", "", "ООО Альфа")
+
+    println("Список контактов в телефонной книге:")
+    contactList.forEach { it.printInfo() }
+}
