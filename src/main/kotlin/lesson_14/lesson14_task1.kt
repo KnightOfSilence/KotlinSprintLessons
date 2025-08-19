41e76a09bcb55c665102a6137bb425e49191d7a1
boss <vodochnik1982@gmail.com>
2025-08-19 22:35:05 +0300

    KS-14-1

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -1,54 +1,55 @@
 package lesson_14
 
 
-open class CargoShip(
-    val name: String = "Грузовое судно",
-    val speed: Int = 20,
-    val tonnage: Int = 50000,
-    val passengersNum: Int = 60,
+open class LinerShip(
+    open val name: String = "Лайнер",
+    open val speed: Int = 20,
+    open val tonnage: Int = 100000,
+    open val passengersCapacity: Int = 6000,
 ) {
-    open fun ShipInfo() {
+    open fun showShipInfo() {
         println(
             """
                 Тип судна: $name
                 Скорость судна: $speed узлов
                 Тоннаж судна: $tonnage тонн
-                Человек на борту: $passengersNum
+                Человек на борту: $passengersCapacity
             """.trimIndent()
         )
     }
 }
 
+class CargoShip(
+    override val name: String = "Грузовое судно",
+    override val speed: Int = 20,
+    override val tonnage: Int = 50000,
+    override val passengersCapacity: Int = 60,
+) : LinerShip(name, speed, tonnage, passengersCapacity)
+
 class IceBreakerLiner(
-    name: String = "Ледокольное судно",
-    speed: Int = 10,
-    tonnage: Int = 15000,
-    passengersNum: Int = 50,
+    override val name: String = "Ледокольное судно",
+    override val speed: Int = 10,
+    override val tonnage: Int = 15000,
+    override val passengersCapacity: Int = 50,
     val isIceBreaker: Boolean = true,
-) : CargoShip(name, speed, tonnage, passengersNum) {
-    override fun ShipInfo() {
-        super.ShipInfo()
-        println(isIceBreaker)
+) : LinerShip(name, speed, tonnage, passengersCapacity) {
+    override fun showShipInfo() {
+        super.showShipInfo()
+        println("Возможность колоть лед: $isIceBreaker")
     }
 }
 
-
-class LinerShip(
-    name: String = "Лайнер",
-    speed: Int = 20,
-    tonnage: Int = 100000,
-    passengersNum: Int = 6000,
-) : CargoShip(name, speed, tonnage, passengersNum)
-
 fun main() {
 
+    val liner = LinerShip()
+    liner.showShipInfo()
+    println()
+
     val cargoShip = CargoShip()
-    cargoShip.ShipInfo()
+    cargoShip.showShipInfo()
+    println()
 
     val iceBreakerLiner = IceBreakerLiner()
-    iceBreakerLiner.ShipInfo()
-
-    val liner = LinerShip()
-    liner.ShipInfo()
+    iceBreakerLiner.showShipInfo()
 }
 
