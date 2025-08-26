589f7eed42860b12dccc65c732cbd341c3c990f5
boss <vodochnik1982@gmail.com>
2025-08-26 13:09:48 +0300

    KS-14-5

--- src/main/kotlin/lesson_14/lesson14_task5.kt
+++ src/main/kotlin/lesson_14/lesson14_task5.kt
@@ -1,48 +1,44 @@
 package lesson_14
 
-import kotlin.random.Random
-
 open class Message(
-    val id: Int = Random.nextInt(),
+    val id: Int,
     val author: String,
     val text: String,
 ) {
-    override fun toString(): String = "[id: $id]$author: $text"
+    override fun toString(): String {
+        return "$author: $text(id: $id)"
+    }
 }
 
 class ChildMessage(
-    parentMessageId: Int,
+    val parentMessageId: Int,
     author: String,
     text: String,
-) : Message(author = author, text = text) {
-    val parentMessageId: Int = parentMessageId
-}
+    id: Int
+) : Message(id, author, text)
 
 class Chat {
     val messages = mutableListOf<Message>()
-    var lastMessageId = 0
+    var nextId = 0
 
     fun addMessage(author: String, text: String) {
-        val message = Message(author = author, text = text)
+        val message = Message(nextId++, author, text)
         messages.add(message)
-        lastMessageId = message.id
     }
 
     fun addThreadMessage(parentMessageId: Int, author: String, text: String) {
-        val childMessage = ChildMessage(parentMessageId, author, text)
+        val childMessage = ChildMessage(parentMessageId, author, text, nextId++)
         messages.add(childMessage)
     }
 
     fun printChat() {
         val groupedMessages =
-            messages.groupBy { if (it is ChildMessage) it.parentMessageId else it.id }
+            messages.filterIsInstance<ChildMessage>().groupBy { it.parentMessageId }
 
-        for ((parentMessageId, childMessages) in groupedMessages) {
-            val parentMessage = messages.find { it.id == parentMessageId }
-            if (parentMessage != null) {
-                println(parentMessage)
-                val childMessages = childMessages.sortedBy { it.id }
-                childMessages.forEach { println(it) }
+        for (message in messages) {
+            if (message !is ChildMessage) {
+                println(message)
+                groupedMessages[message.id]?.forEach { childMessage -> println("\t $childMessage") }
             }
         }
     }
@@ -51,10 +47,10 @@ class Chat {
 fun main() {
     val chat = Chat()
     chat.addMessage("Иванов", "Привет!")
-    chat.addThreadMessage(chat.lastMessageId, "Петров", "Приветствую!")
+    chat.addThreadMessage(0, "Петров", "Приветствую!")
     chat.addMessage("Сидоров", "Как дела?")
-    chat.addThreadMessage(chat.lastMessageId, "Иванов", "Хорошо, а у тебя?")
-    chat.addThreadMessage(chat.lastMessageId, "Сидоров", "Что нового?")
+    chat.addThreadMessage(0, "Иванов", "Хорошо, а у тебя?")
+    chat.addThreadMessage(0, "Сидоров", "Что нового?")
     chat.printChat()
 }
 
