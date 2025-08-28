810e82c935f61528c45dce1100c56125a11fb950
boss <vodochnik1982@gmail.com>
2025-08-28 23:05:04 +0300

    KS-16-5. Про комментарии принято.

--- src/main/kotlin/lesson_16/lesson16_task5.kt
+++ src/main/kotlin/lesson_16/lesson16_task5.kt
@@ -3,15 +3,16 @@ package lesson_16
 class Player(
     private val name: String,
     private var health: Int = 100,
-    var punchPower: Int = 20,
+    private var punchPower: Int = 20,
 ) {
     fun getHealth(): Int {
         return health
     }
 
     fun getDamage(damage: Int = 20): Int {
+        health -= damage
+
         if (health > 0) {
-            health -= damage
             println("$name получил $damage урона. Осталось $health здоровья.")
         } else {
             death()
@@ -20,9 +21,11 @@ class Player(
     }
 
     fun heal(cure: Int = 20): Int {
-        health += cure
+        if (health > 0) {
+            health += cure
+        }
         println("$name восстановился на $cure единиц здоровья. Осталось $health здоровья.")
-       return health
+        return health
     }
 
     private fun death() {
