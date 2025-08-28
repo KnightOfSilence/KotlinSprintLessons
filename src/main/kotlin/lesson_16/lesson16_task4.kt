6a7cba7c654d84bb727e7d8286b36055c6c018ce
BOSS <vodochnik1982@gmail.com>
2025-08-28 14:41:34 +0300

    KS-16-4

--- src/main/kotlin/lesson_16/lesson16_task4.kt
+++ src/main/kotlin/lesson_16/lesson16_task4.kt
@@ -1,26 +1,30 @@
 package lesson_16
 
-import kotlin.io.println
 
 class Order(
     private val number: Int,
-    var isReady: Boolean = false,
+    private var isReady: Boolean = false,
 ) {
     fun getOrderNumber(): Int {
         return number
     }
 
-    fun changeOrderStatus(order: Boolean) {
+    fun getOrderStatus(): Boolean {
+        return isReady
+    }
+
+    private fun changeOrderStatus(order: Boolean) {
         isReady = order
+    }
+    fun queryToManager(order: Order, query: Boolean) {
+        order.changeOrderStatus(query)
     }
 }
 
-fun queryToManager(order: Order, query: Boolean) {
-    order.changeOrderStatus(query)
-}
+
 
 fun main() {
     val order = Order(1)
-    queryToManager(order, true)
-    println("Заказ ${order.getOrderNumber()} готов: ${order.isReady}")
+    order.queryToManager(order, true)
+    println("Заказ ${order.getOrderNumber()} готов: ${order.getOrderStatus()}")
 }
