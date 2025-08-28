295ca9f480267b53714b3b3f17f2bde2be9c1981
BOSS <vodochnik1982@gmail.com>
2025-08-28 14:32:00 +0300

    KS-16-3

--- src/main/kotlin/lesson_16/lesson16_task3.kt
+++ src/main/kotlin/lesson_16/lesson16_task3.kt
@@ -1,22 +1,25 @@
 package lesson_16
 
+import kotlin.io.println
+
 class User(
     private val login: String,
     private val password: String
 ) {
-    fun isPasswordCorrect() {
-        println("Введите пароль")
-        val userPassword: String = readln()
-
-        if (userPassword == password) {
-            println("Вход выполнен успешно")
-        } else {
-            println("Неверный пароль")
-        }
+    fun isPasswordCorrect(userPassword: String): Boolean {
+        return userPassword == password
     }
 }
 
 fun main() {
     val user = User("admin", "qwerty")
-    user.isPasswordCorrect()
+
+    println("Введите пароль")
+    val userPassword: String = readln()
+
+    if (user.isPasswordCorrect(userPassword)) {
+        println("Вход выполнен успешно")
+    } else {
+        println("Неверный пароль")
+    }
 }
