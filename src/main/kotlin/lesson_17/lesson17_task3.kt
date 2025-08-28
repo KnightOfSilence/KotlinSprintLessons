46c85abed504240c4a5720d6b0df9a16a9d5d092
boss <vodochnik1982@gmail.com>
2025-08-29 00:23:39 +0300

    KS-17-3

---
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -0,0 +1,29 @@
+package lesson_17
+
+private const val HIDDEN_DOCUMENTS = "скрытая папка"
+private const val NUMBER_OF_FILES = 0
+
+class Package(
+    _name: String,
+    _files: Int,
+    var isSecretFiles: Boolean,
+) {
+    val files = _files
+        get() = if (isSecretFiles) NUMBER_OF_FILES else field
+
+    val name = _name
+        get() = if (isSecretFiles) HIDDEN_DOCUMENTS else "$field количество файлов $files"
+}
+
+fun main() {
+
+    val myFiles = Package("favorites", 70, false)
+    println(myFiles.name)
+    println()
+    myFiles.isSecretFiles = true
+    println("${myFiles.name} количество файлов ${myFiles.files}")
+    println()
+    myFiles.isSecretFiles = false
+    println(myFiles.name)
+}
+
