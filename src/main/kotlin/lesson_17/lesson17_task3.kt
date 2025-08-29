07dbf299a82657354a2561a452c58071cf6ca4ea
BOSS <vodochnik1982@gmail.com>
2025-08-29 20:07:51 +0300

    KS-17-3

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -3,27 +3,26 @@ package lesson_17
 private const val HIDDEN_DOCUMENTS = "скрытая папка"
 private const val NUMBER_OF_FILES = 0
 
-class Package(
+class Folder(
     _name: String,
     _files: Int,
-    var isSecretFiles: Boolean,
+    var isSecret: Boolean,
 ) {
     val files = _files
-        get() = if (isSecretFiles) NUMBER_OF_FILES else field
+        get() = if (isSecret) NUMBER_OF_FILES else field
 
     val name = _name
-        get() = if (isSecretFiles) HIDDEN_DOCUMENTS else "$field количество файлов $files"
+        get() = if (isSecret) HIDDEN_DOCUMENTS else field
 }
 
 fun main() {
 
-    val myFiles = Package("Избранное", 234, false)
+    val myFiles = Folder("Избранное", 234, false)
     println(myFiles.name)
     println()
-    myFiles.isSecretFiles = true
-    println("${myFiles.name} количество файлов ${myFiles.files}")
+    myFiles.isSecret = true
+    println("${myFiles.name}, количество файлов ${myFiles.files}")
     println()
-    myFiles.isSecretFiles = false
-    println(myFiles.name)
+    myFiles.isSecret = false
+    println("${myFiles.name}, количество файлов ${myFiles.files}")
 }
-
