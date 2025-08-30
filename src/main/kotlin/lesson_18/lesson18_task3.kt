127e002fe964fb7593b432791d2720b58c4a9f9c
boss <vodochnik1982@gmail.com>
2025-08-30 21:32:14 +0300

    KS-18-3

--- src/main/kotlin/lesson_18/lesson18_task3.kt
+++ src/main/kotlin/lesson_18/lesson18_task3.kt
@@ -1,32 +1,25 @@
 package lesson_18
 
-open class Animal {
-    open fun sleep() = "спит"
-    open fun eat() = "ест"
-}
+abstract class Animal {
+    abstract val name: String
+    abstract val food: String
 
-class Fox(private val name: String, private val food: String) : Animal() {
-    override fun eat(): String {
+    fun sleep() = "спит"
+    fun eat(): String {
         return "$name ест $food"
     }
 }
 
-class Dog(private val name: String, private val food: String) : Animal() {
-    override fun eat(): String {
-        return "$name ест $food"
-    }
-}
+class Fox(override val name: String, override val food: String) : Animal()
 
-class Cat(private val name: String, private val food: String) : Animal() {
-    override fun eat(): String {
-        return "$name ест $food"
-    }
-}
+class Dog(override val name: String, override val food: String) : Animal()
 
-fun main(){
+class Cat(override val name: String, override val food: String) : Animal()
+
+fun main() {
     val fox = Fox("Лиса", "ягоды")
     val dog = Dog("Собака", "кости")
     val cat = Cat("Кошка", "рыбу")
     val animals = listOf<Animal>(fox, dog, cat)
-    animals.forEach { println(it.eat())}
+    animals.forEach { println(it.eat()) }
 }
