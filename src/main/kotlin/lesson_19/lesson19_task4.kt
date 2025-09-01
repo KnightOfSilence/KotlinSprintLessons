57d3a2b383dcb436e3ea4eebb753aecd2c9246aa
boss <vodochnik1982@gmail.com>
2025-09-01 22:32:11 +0300

    KS-19-4

--- src/main/kotlin/lesson_19/lesson19_task4.kt
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -1,33 +1,30 @@
 package lesson_19
 
 enum class Ammunition(val damage: Int? = null) {
-    BLUE(5), GREEN(10), RED(20), EMPTY()
+    BLUE(5), GREEN(10), RED(20),
 }
 
-class Tank(name: String) {
+class Tank(name: String, private var ammunition: Ammunition? = null) {
     fun weaponLoad(ammunition: Ammunition): String {
         when (ammunition) {
             Ammunition.BLUE -> return "Заряжен ${Ammunition.BLUE} снаряд с уроном ${Ammunition.BLUE.damage}."
             Ammunition.GREEN -> return "Заряжен ${Ammunition.GREEN} снаряд с уроном ${Ammunition.GREEN.damage}."
             Ammunition.RED -> return "Заряжен ${Ammunition.RED} снаряд с уроном ${Ammunition.RED.damage}."
-            Ammunition.EMPTY -> return "Орудие не заряжено!"
         }
+    }
+
+
+    fun shotFire(damage: Int?) {
+        if (damage == null) println("Не заряжен.") else println("Произведен выстрел с уроном $damage")
     }
 }
 
-fun shotFire(damage: Int?) {
-    if (damage == null) println("Не заряжен.") else println("Произведен выстрел с уроном $damage")
-}
-
-fun main(){
+fun main() {
     val tank = Tank("Т-34")
     println(tank.weaponLoad(Ammunition.BLUE))
-    shotFire(Ammunition.BLUE.damage)
-    println(tank.weaponLoad(Ammunition.EMPTY))
-    shotFire(Ammunition.EMPTY.damage)
+    tank.shotFire(Ammunition.BLUE.damage)
     println(tank.weaponLoad(Ammunition.GREEN))
-    shotFire(Ammunition.GREEN.damage)
+    tank.shotFire(Ammunition.GREEN.damage)
     println(tank.weaponLoad(Ammunition.RED))
-    shotFire(Ammunition.RED.damage)
+    tank.shotFire(Ammunition.RED.damage)
 }
-
