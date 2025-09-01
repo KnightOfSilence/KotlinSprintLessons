7fbcb99aae6d69289697ee393c407db992b3572c
boss <vodochnik1982@gmail.com>
2025-09-01 22:22:11 +0300

    KS-19-1. Теперь названия рыбок на русском языке.

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -1,15 +1,15 @@
 package lesson_19
 
-enum class AquariumFish {
-    ГУППИ,
-    СКАЛЯРИЯ,
-    ЗОЛОТАЯ_РЫБКА,
-    ПЕТУШОК,
+enum class AquariumFish(var nameFish: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Сколярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок")
 }
 
 fun snowFishNames() {
     for (fish in AquariumFish.entries) {
-        println(fish)
+        println(fish.nameFish)
     }
 }
 
