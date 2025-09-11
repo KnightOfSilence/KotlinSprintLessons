ffd0386c988de48b7c87bcd5d12926cc9d9e4fd6
boss <vodochnik1982@gmail.com>
2025-09-11 10:39:25 +0300

    KS-21-4

---
+++ src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -0,0 +1,18 @@
+package lesson_21
+
+import java.io.File
+
+fun File.writeToFile(text: String) {
+
+    createNewFile()
+    val userText = text.lowercase()
+    val lines: MutableList<String> = this.readLines().toMutableList()
+    lines.add(0, userText)
+    writeText(lines.toString())
+}
+
+fun main() {
+    val newFile = File("textFile")
+    newFile.writeToFile("Text1")
+    newFile.writeToFile("Text2")
+}
